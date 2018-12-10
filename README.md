3cea2bc3da616187caa07b94980eb8035171e9a6
rymlaabiyad <rym.laab@gmail.com>
2018-12-10 18:29:30 +0100

    Merging the two dataframes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Setup
-* Open the init.cfg file and change both the path and the file name if needed. Keep that file in the same directory as 'data_retriever.py'
+* Open the init.cfg file and change both the path and the file name if needed. Keep that file in the same directory as 'main.py'
 * Open a command line, go to the directory where you put 'requirements.txt', and run :
     conda install --yes --file requirements.txt
     OR
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data_retriever import retrieveData
 from data_prep import prepareData
 
 from configparser import ConfigParser
-
+import pandas as pd
 
 
 config = ConfigParser()
@@ -13,7 +13,13 @@ dataFile = config['RESOURCES']['dataFile']
 # We retrieve a dataframe corresponding to our data file
 # and an array containing only the reviews
 df, reviews = retrieveData(path + dataFile)
+print(df.shape)
 
 # We add to it columns. One for each feature (relevant word).
 # If a review contains a feature, the corresponding column will have value 1, else 0.
-df_features = prepareData(df, reviews)
+df_features = pd.concat([df, prepareData(df, reviews)], axis=1)
+print(df_features.shape)
+#json = df_features.to_json(orient='records')
+#f = open(path + 'features.json', 'w')
+#f.write(json[1:-1])
+#f.close()
